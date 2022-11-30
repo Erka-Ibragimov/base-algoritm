b06d8c06c98d63c4f2f45d6faff1a1877e6734d8
Erka-Ibragimov <erkaibragimov@gmail.com>
2022-11-30 17:44:07 +0500

    q

--- 2_binary_search.js
+++ 2_binary_search.js
@@ -22,5 +22,19 @@ function binarySearch(num, array) {
     }
     return position;
 }
-console.log(binarySearch(9, array));;
+
+function recursiveBinarySearch(array,item, start, end) {
+    let middle = Math.floor((start + end)/2);
+    count += 1;
+    if(item === array[middle]){
+        return middle;
+    }
+    if(item < array[middle]){
+        return recursiveBinarySearch(array,item, start, middle-1);
+    } else{
+        return recursiveBinarySearch(array,item,middle+1,end)
+    }
+}
+// console.log(binarySearch(9, array));;
+console.log(recursiveBinarySearch(array,6,0,array.length));
 console.log(count);
